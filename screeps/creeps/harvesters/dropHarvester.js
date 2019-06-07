ae3c4a9853cfa2ba5c0e66667eeebc55c36fbda6
Tyler Carver <carver230620@cua.edu>
2019-06-07 17:33:45 -0600

    Assigning resources to the drop harvester.

--- screeps/creeps/harvesters/dropHarvester.js
+++ screeps/creeps/harvesters/dropHarvester.js
@@ -14,10 +14,18 @@ DropHarvester.prototype.act = function() {
 
 		if (this.state === "harvesting") {
 
-			var resource = this.creep.pos.findClosestByPath(FIND_SOURCES);
+			if (this.memory.resourceId) {
+				var resource = Game.getObjectById(this.memory.resourceId);
+			} else {
+				var resource = this.creep.pos.findClosestByPath(FIND_SOURCES);
+			}
 
-			if (this.creep.harvest(resource) == ERR_NOT_IN_RANGE) {
-				this.creep.moveTo(resource);
+			if (resource) {
+				if (this.creep.harvest(resource) == ERR_NOT_IN_RANGE) {
+					this.creep.moveTo(resource);
+				}
+			} else {
+				debug.warning(`${this.type} no resource found, resourceId: ${this.memory.resourceId}`);
 			}
 		}
 	}
@@ -32,10 +40,44 @@ DropHarvester.initializeSpawnCreepMemory = function(room, spawn, creepsSpawnRule
 	var creepMemory = {
 		type: "dropHarvester",
 		bodyPartsType: "workDropper",
-		state: "harvesting",
+		state: "harvesting"
+	}
+
+	if (room.find) {
+
+		// Evenly distribute creeps
+		for (var count = 1; count <= 5; count++) {
+
+			var resource = room.find(FIND_SOURCES, {
+				filter: resource => countDropHarvestersAtResource(resource.resourceId) < count
+			});
+
+			if (resource) {
+
+				creepMemory.resourceId = resource.resourceId;
+				break;
+			}
+		}
 	}
 
 	return creepMemory;
+}
+
+function countDropHarvestersAtResource(resourceId) {
+
+	var countCreeps = _.reduce(Memory.creeps, (countCreeps, creepMemory) => {
+
+		if (creepMemory.type === "dropHarvester") {
+
+			if (creepMemory.resourceId === resourceId) {
+				countCreeps++;
+			}
+		}
+
+		return countCreeps;
+	}, 0);
+
+	return countCreeps;
 }
 
 
